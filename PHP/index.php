da15bba550356d2728727df963640b048027f58c
Júlia Pinheiro <julia_jgp@hotmail.com>
2021-10-16 20:42:09 -0300

    Add files via upload

---
+++ PHP/index.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+    </head>
+
+    <body>
+
+        <?php 
+            echo 'Utilizando a tag padrão';
+        ?>
+        <br/>
+
+        <?= 'Jujuju' ?>
+
+        <? echo 'Julinds' ?>
+    </body>
+
+</html>
