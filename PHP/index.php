74253a5e72e34c6345945895783d44dcd5550cdc
Júlia Pinheiro <julia_jgp@hotmail.com>
2021-10-18 18:49:04 -0300

    Add files via upload

--- PHP/index.php
+++ PHP/index.php
@@ -5,13 +5,20 @@
     <body>
 
         <?php 
-            echo 'Utilizando a tag padrão';
+
+            echo 'Comando echo<br>';
+            
+            print 'Comando print';
+            //comentario de 1 linha
+            #comentario de 1 linha, estilo shell
+            /* Comentario
+               de multiplas linhas
+             */
+
+             //declaração de variavel
+             $nome;
         ?>
-        <br/>
 
-        <?= 'Jujuju' ?>
-
-        <? echo 'Julinds' ?>
     </body>
 
 </html>
