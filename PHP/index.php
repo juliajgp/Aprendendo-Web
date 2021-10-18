8fc60c71824c2984aeba1aba15be3c4b9c001036
Júlia Pinheiro <julia_jgp@hotmail.com>
2021-10-18 19:05:19 -0300

    Add files via upload

--- PHP/index.php
+++ PHP/index.php
@@ -6,18 +6,24 @@
 
         <?php 
 
-            echo 'Comando echo<br>';
-            
-            print 'Comando print';
-            //comentario de 1 linha
-            #comentario de 1 linha, estilo shell
-            /* Comentario
-               de multiplas linhas
-             */
+            //variaveis
+            $nome = "Júlia Gomes";
+            $idade = 25;
+            $peso = 57;
+            //se for true, aparece 1 na impressão, se for false, não aparece nada.
+            $fuma = false;
 
-             //declaração de variavel
-             $nome;
+            //concatenação
+            echo "Olá, $nome, tudo bem??";
+            //echo "Olá, '.$nome.', tudo bem??';
         ?>
+
+        <h1>Ficha Cadastral</h1>
+        <br>
+        <p>Nome: <?= $nome?></p>
+        <p>Idade: <?= $idade?></p>
+        <p>Peso: <?= $peso?></p>
+        <p>Fumante: <?= $fuma ?></p>
 
     </body>
 
