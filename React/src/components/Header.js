3ef4929c5af9c124d55046d560827c70279a341b
Júlia Pinheiro <julia_jgp@hotmail.com>
2021-11-01 19:23:53 -0300

    Add files via upload

--- React/src/components/Header.js
+++ React/src/components/Header.js
@@ -3,15 +3,16 @@ import Button from './Button'
 
 //ou substituir (props) por ({title})
 // e {props.title} por {title}
-const Header = ({title}) => {
-
-    const onClick = (e) => {
-    }
+const Header = ({ title, onAdd, showAdd }) => {
 
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Add' onClick={onClick}/>
+            <Button
+                color={showAdd ? 'red' : 'green'}
+                text={showAdd ? 'Close' : 'Add'}
+                onClick={onAdd}
+            />
         </header>
     )
 }
