752b3fdbced50a95e4e5e55a21166a8bee3625c2
Júlia Pinheiro <julia_jgp@hotmail.com>
2023-09-29 12:39:50 -0300

    Add files via upload

---
+++ PHP/OO/oo_heranca.php
@@ -0,0 +1,52 @@
+<?php
+
+    class Veiculo {
+        public $placa = null;
+        public $cor = null;
+
+        function acelerar() {
+            echo 'Acelerou';
+        }
+    }
+
+    class Carro extends Veiculo {
+        public $teto_solar = true;
+
+        function __construct($placa, $cor) {
+            $this->placa = $placa;
+            $this->cor = $cor;
+        }
+
+        function abrirTetoSolar() {
+            echo 'Abrir teto Solar';
+        }
+
+        function alterarPosicaoVolante() {
+            echo 'Alterar posição volante';
+        }
+    }
+
+    class Moto extends Veiculo {
+        public $contraPesoGuidao = true;
+
+        function __construct($placa, $cor) {
+            $this->placa = $placa;
+            $this->cor = $cor;
+        }
+
+        function empinar() {
+            echo 'Empinar';
+        }
+    }
+
+    $carro = new Carro('ABC1234', 'Branco');
+    $moto = new Moto('DEF1122', 'Preto');
+
+    echo '<pre>';
+    print_r($carro);
+    print_r($moto);
+    echo '</pre>';
+
+    $carro->acelerar();
+
+?>
