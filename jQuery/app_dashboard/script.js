8cb97bbb614327e75dfaf6fa2cb1b01cdb455bb3
Júlia Pinheiro <julia_jgp@hotmail.com>
2023-10-10 15:39:58 -0300

    Add files via upload

--- jQuery/app_dashboard/script.js
+++ jQuery/app_dashboard/script.js
@@ -25,4 +25,22 @@ $(document).ready(() => {
 			$('#pagina').html(data)
 		})
 	})
+
+	//ajax
+	$('#competencia').on('change', e => {
+		let competencia = $(e.target).val()
+
+		$.ajax({
+			type: 'GET',
+			url: 'app.php',
+			data: `competencia=${competencia}`,
+			dataType: 'json',
+			success: dados => {
+				$('#numeroVendas').html(dados.numeroVendas)
+				$('#totalVendas').html(dados.totalVendas)
+				//console.log(dados)
+			},
+			error: erro => {console.log(erro)}
+		})
+	})
 })
