ab42bdb21b164b48bb47bd74e01f06b81bc49c8e
Júlia Pinheiro <julia_jgp@hotmail.com>
2023-11-23 03:18:22 -0300

    Add files via upload

--- projeto twitter clone/App/Models/Usuario.php
+++ projeto twitter clone/App/Models/Usuario.php
@@ -32,8 +32,34 @@ class Usuario extends Model {
     }
 
     //validar se um cadastro pode ser feito
+    public function validarCadastro() {
+        $valido = true;
 
-    //recuperar um usuario por e-mail
+        if(strlen($this->__get('nome')) < 3) {
+            $valido = false;
+        }
+
+        if(strlen($this->__get('email')) < 3) {
+            $valido = false;
+        }
+
+        if(strlen($this->__get('senha')) < 3) {
+            $valido = false;
+        }
+
+        return $valido;
+    }
+
+    //verificar se o usuario ja foi inserido
+    public function getUsuarioPorEmail() {
+        $query = "select nome, email from usuarios where email = :email";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':email', $this->__get('email'));
+        $stmt->execute();
+
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
 
 }
 
